d7681a200f02e802bb3ec35ae299a41bb6273e98
LF-Vigneux <156103879+LF-Vigneux@users.noreply.github.com>
2026-06-01 22:31:04 -0400

    deniuser

--- denoiser/classical_denoiser.py
+++ denoiser/classical_denoiser.py
@@ -1,10 +1,12 @@
 import torch
 import torch.nn as nn
 
+# https://github.com/rajatguptakgp/image_enhancement_techniques
+
 
 class Denoiser(nn.Module):
     # No pooling because we want to keep the features
-    def __init__(self, image_size: int = 16, RGB: bool = False, num_layers: int = 4):
+    def __init__(self, image_size: int = 16, RGB: bool = False):
         super().__init__()
         if RGB:
             self.encoder = nn.Sequential(
@@ -50,6 +52,70 @@ class Denoiser(nn.Module):
                 nn.Conv2d(image_size, 1, 3, padding=1),
                 nn.Sigmoid(),
             )
+
+    def forward(self, x):
+        z = self.encoder(x)
+        return self.decoder(z)
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = nn.Sequential(
+            # 3 x 32 x 32
+            nn.Conv2d(3, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(2),  # 64 x 16 x 16
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.MaxPool2d(2),  # 128 x 8 x 8
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+        )
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.decoder = nn.Sequential(
+            # 256 x 8 x 8
+            nn.ConvTranspose2d(
+                256, 128, kernel_size=4, stride=2, padding=1
+            ),  # 128 x 16 x 16
+            nn.ReLU(),
+            nn.BatchNorm2d(128),
+            nn.ConvTranspose2d(
+                128, 64, kernel_size=4, stride=2, padding=1
+            ),  # 64 x 32 x 32
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 3, 3, padding=1),
+            nn.Sigmoid(),  # outputs in [0,1]
+        )
+
+    def forward(self, x):
+        return self.decoder(x)
+
+
+class FullDenoiser(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = Encoder()
+        self.decoder = Decoder()
 
     def forward(self, x):
         z = self.encoder(x)
